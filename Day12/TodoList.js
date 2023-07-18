351976f1b412f9ce87ed6e943a92ed3e127fc555
SeoJeongchan <wjdcks822@gmail.com>
2023-07-18 17:46:09 +0900

    Day12 todoList list 생성

---
+++ Day12/TodoList.js
@@ -0,0 +1,24 @@
+// params.$target - 해당 컴포넌트가 추가가 될 DOM 엘리먼트
+// params.initialState - 해당 컴포넌트의 초기 상태
+
+function TodoList({$target, initialState}) {
+    const $todoList = document.createElement('div');
+    $target.appendChild($todoList);
+
+    this.state = initialState;
+
+    this.setState = nextState => {
+        this.state = nextState;
+        this.render();
+    }
+
+    this.render = () => {
+        $todoList.innerHTML = `
+            <ul>
+                ${this.state.map(({ text }) => `<li>${text}</li>`).join('')}
+            </ul>        
+        `
+    }
+
+    this.render();
+}
