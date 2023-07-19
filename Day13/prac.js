63134a50ffbd5096e5be91f4841549381e2ef6e2
SeoJeongchan <wjdcks822@gmail.com>
2023-07-19 17:33:36 +0900

    Day13 GitHub Copilot 사용해보기

---
+++ Day13/prac.js
@@ -0,0 +1,18 @@
+// copilot practice
+
+// 문제1 banana quiz 
+console.log("b"+"a"+ +"a"+"a");
+console.log(("b"+"a"+ +"a"+"a").toLowerCase());
+
+// 문제2 input A and B, output A+B, A-B, A*B
+
+// input A in first line, input B in second line
+// input is not exceed 1,000 decimal digits
+// both positive and negative numbers are possible
+// output A+B in first line, A-B in second line, A*B in third line
+// each output type is integer
+function calculateAB(a, b) {
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+}
