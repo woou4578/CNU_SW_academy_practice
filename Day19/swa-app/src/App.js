5df2a2599bc9502eccafd15ec5a236958194510d
SeoJeongchan <wjdcks822@gmail.com>
2023-07-28 15:44:56 +0900

    Day19 useEffect 학습

--- Day19/swa-app/src/App.js
+++ Day19/swa-app/src/App.js
@@ -1,34 +1,61 @@
-import './App.css';
+import { useEffect, useState } from "react";
+// import './App.css';
 // import Logo from './components/Logo';
 // import Paragraph from './components/Paragraph';
-// import {useState} from "react";
 // import Board from './components/Board';
-import Counter from './components/Counter';
-import {useState} from 'react';
+// import Counter from './components/Counter';
 
 function App() {
+	// useEffect는 무언가 변화가 있을 때
+	// 감지하여 반응하는 Hook
+	const [count, setcount] = useState(0);
+
+	useEffect(() => {
+		console.log(`Clicked ${count} times.`);
+	}, [count]);
+
+	// 배열에 아무것도 넣지 않으면, 컴포넌트가 처음으로 로드될 때 실행
+	useEffect(() => {
+		console.log("Component Loaded");
+		const handleScroll = () => {
+			console.log(window.scrollY);
+		};
+
+		document.addEventListener("scroll", handleScroll);
+		// return으로 반환한 함수는 컴포넌트가 제거될때 실행
+		return () => document.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	return (
+		<div>
+			<div>You Clicked {count} times.</div>
+			<button onClick={() => setcount(count + 1)}>+</button>
+			<div style={{height:2000}}></div>
+		</div>
+	);
+
 	// 상태와 이벤트 바인딩
 	// 1. 컴포넌트에서 지역 상태 관리하는 법
 	// 2. 컴포넌트에 이벤트 바인딩하기
 	// 3. 부모 컴포넌트에게 메시지 전달하기
-	const [totalCount, setTotalCount] = useState(0);
-	return (
-		<div>
-			TotalCount: {totalCount}
-			<Counter 
-				onIncrease={() => { setTotalCount(totalCount+1)}}
-				onDecrease={() => { setTotalCount(totalCount-1)}}
-			/>
-			<Counter 
-				onIncrease={() => { setTotalCount(totalCount+1)}}
-				onDecrease={() => { setTotalCount(totalCount-1)}}
-			/>
-			<Counter 
-				onIncrease={() => { setTotalCount(totalCount+1)}}
-				onDecrease={() => { setTotalCount(totalCount-1)}}
-			/>
-		</div>
-	);
+	// const [totalCount, setTotalCount] = useState(0);
+	// return (
+	// 	<div>
+	// 		TotalCount: {totalCount}
+	// 		<Counter
+	// 			onIncrease={() => { setTotalCount(totalCount+1)}}
+	// 			onDecrease={() => { setTotalCount(totalCount-1)}}
+	// 		/>
+	// 		<Counter
+	// 			onIncrease={() => { setTotalCount(totalCount+1)}}
+	// 			onDecrease={() => { setTotalCount(totalCount-1)}}
+	// 		/>
+	// 		<Counter
+	// 			onIncrease={() => { setTotalCount(totalCount+1)}}
+	// 			onDecrease={() => { setTotalCount(totalCount-1)}}
+	// 		/>
+	// 	</div>
+	// );
 
 	// 분기와 반복
 	// const [visible, setVisible] = useState(false);
@@ -48,10 +75,10 @@ function App() {
 	// return <div>
 	// 	<button onClick={() => setVisible(!visible)}>Toggle</button>
 	// 	{visible && <h1>논리곱 연산자를 통해 쉽게 JSX 렌더링 여부를 결정할 수 있다.</h1> }
-		
+
 	// 	{visible ? (
 	// 		<Board articles={articles}/>
-	// 	) : ( 
+	// 	) : (
 	// 		<p>게시판을 보려면 Toggle 버튼 클릭하기</p>
 	// 	)}
 	// </div>;
@@ -63,30 +90,30 @@ function App() {
 	// div로 묶거나 fragment로 처리
 	// tag가 없으면 fragment
 	// React.Fragment로도 가능
-//   return (
-// 		<div className="App">
-// 			<header className="App-header">
-//         <Logo size={300}/>
-//         <Logo/>
-//         <Logo/>
+	//   return (
+	// 		<div className="App">
+	// 			<header className="App-header">
+	//         <Logo size={300}/>
+	//         <Logo/>
+	//         <Logo/>
 
-// 				<Paragraph>
-// 					Edit <code>src/App.js</code> and save to reload.
-// 				</Paragraph>
-//         <Paragraph size={14} color="blue">
-//           I'm Blue
-//         </Paragraph>
-// 				<a
-// 					className="App-link"
-// 					href="https://reactjs.org"
-// 					target="_blank"
-// 					rel="noopener noreferrer"
-// 				>
-// 					Learn React
-// 				</a>
-// 			</header>
-// 		</div>
-//   );
+	// 				<Paragraph>
+	// 					Edit <code>src/App.js</code> and save to reload.
+	// 				</Paragraph>
+	//         <Paragraph size={14} color="blue">
+	//           I'm Blue
+	//         </Paragraph>
+	// 				<a
+	// 					className="App-link"
+	// 					href="https://reactjs.org"
+	// 					target="_blank"
+	// 					rel="noopener noreferrer"
+	// 				>
+	// 					Learn React
+	// 				</a>
+	// 			</header>
+	// 		</div>
+	//   );
 }
 
 export default App;
