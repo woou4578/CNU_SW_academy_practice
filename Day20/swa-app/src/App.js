c442ad7f0c095bc37ddf2ead03f2bc891d761610
SeoJeongchan <wjdcks822@gmail.com>
2023-07-31 13:38:51 +0900

    Day20 useCallback 사용

--- Day20/swa-app/src/App.js
+++ Day20/swa-app/src/App.js
@@ -1,17 +1,32 @@
 // import logo from "./logo.svg";
 // import "./App.css";
-
-import Box from "./components/Box";
-import {useState} from "react";
+// import Box from "./components/Box";
+import { useState, useCallback } from "react";
+import Checkbox from "./components/Checkbox";
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [foodOn, setFoodOn] = useState(false);
+	const [clothesOn, setClothesOn] = useState(false);
+	const [shelterOn, setShelterOn] = useState(false);
+
+	const foodChange = useCallback((e) => setFoodOn(e.target.checked), []);
+	const clothesChange = useCallback((e) => setClothesOn(e.target.checked), []);
+	const shelterChange = useCallback((e) => setShelterOn(e.target.checked), []);
+
 
 	return <div>
-    {count}
-    <button onClick={() => setCount(count+1)}>+</button>
-    <Box />
-  </div>;
+		<Checkbox label="Food" on={foodOn} onChange={foodChange}/>
+		<Checkbox label="Clothes" on={clothesOn} onChange={clothesChange}/>
+		<Checkbox label="Shelter" on={shelterOn} onChange={shelterChange}/>
+	</div>;
+
+//   const [count, setCount] = useState(0);
+
+// 	return <div>
+//     {count}
+//     <button onClick={() => setCount(count+1)}>+</button>
+//     <Box />
+//   </div>;
 
 	// return (
 	//   <div className="App">
