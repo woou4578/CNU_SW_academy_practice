b06a8f5127fd4efb85a0bbed679f6c1b885b39bb
SeoJeongchan <wjdcks822@gmail.com>
2023-07-31 15:28:24 +0900

    Storybook 활용

--- Day20/swa-app/src/components/Box.js
+++ Day20/swa-app/src/components/Box.js
@@ -1,21 +1,20 @@
-// import React from 'react';
-// const Box = React.memo(() => {
-//     const style = {
-//         width: 100,
-//         height: 100,
-//         backgroundColor: "blue"
-//     };
-//     return <div style={style} />;
-// });
-
-// export default Box;
-
-import styled from "@emotion/styled";
-
-const Box = styled.div`
-    width: 100px;
-    height: 100px;
-    background-color: blue;
-`;
+const Box = ({width = 100, height = 100, backgroundColor = "red"}) => {
+    const style = {
+        width,
+        height,
+        backgroundColor,
+    };
+    return <div style={style} />;
+};
 
 export default Box;
+
+// import styled from "@emotion/styled";
+
+// const Box = styled.div`
+//     width: 100px;
+//     height: 100px;
+//     background-color: blue;
+// `;
+
+// export default Box;
