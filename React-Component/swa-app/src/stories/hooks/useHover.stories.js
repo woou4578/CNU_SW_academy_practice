f5243a0fdbc9f2c58dd42d8eae94baea46a301e7
SeoJeongchan <wjdcks822@gmail.com>
2023-08-09 17:37:42 +0900

    Day28 Hook-useHover 실습

---
+++ React-Component/swa-app/src/stories/hooks/useHover.stories.js
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+import useHover from "../../hooks/useHover";
+export default {
+	title: "Hook/useHover",
+};
+
+const Box = styled.div`
+	width: 100px;
+	height: 100px;
+	background-color: red;
+`;
+
+export const Default = () => {
+	const [ref, hover] = useHover();
+	return (
+		<>
+			<Box ref={ref} />
+			{hover ? <div>Tool Tip!</div> : null}
+		</>
+	);
+};
