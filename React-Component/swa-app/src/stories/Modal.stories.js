4266add28ea5dc1264c7814e70c8ebe365c7087a
SeoJeongchan <wjdcks822@gmail.com>
2023-08-14 13:53:09 +0900

    Day31 Component-Modal 실습

---
+++ React-Component/swa-app/src/stories/Modal.stories.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Modal from "../components/Modal";
+
+export default {
+	title: "Component/Modal",
+	component: Modal,
+};
+
+export const Default = () => {
+	const [visible, setVisible] = useState(false);
+
+	return (
+		<div>
+			<button onClick={() => setVisible(true)}>Show Modal</button>
+			<Modal visible={visible} onClose={() => setVisible(false)}>Hi!</Modal>
+		</div>
+	);
+};
