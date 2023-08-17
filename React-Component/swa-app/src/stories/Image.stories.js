2eb16bbee1342366383cac0688cb6f20b8ef0154
SeoJeongchan <wjdcks822@gmail.com>
2023-08-17 11:36:22 +0900

    Day34 Component-Image 실습(Error 해결)

--- React-Component/swa-app/src/stories/Image.stories.js
+++ React-Component/swa-app/src/stories/Image.stories.js
@@ -4,37 +4,60 @@ export default {
 	title: "Component/Image",
 	component: Image,
 	argTypes: {
+		lazy: {
+			control: { type: "boolean" },
+		},
 		block: {
-			defaultValue: false,
 			control: { type: "boolean" },
 		},
 		src: {
 			type: { name: "string", require: true },
-			defaultValue: "https://picsum.photos/200",
 			control: { type: "text" },
 		},
+		placeholder: {
+			type: { name: "string" },
+			control: { type: "text" },
+		},
+		threshold: {
+			type: { name: "number" },
+			control: { type: "number" },
+		},
 		width: {
-			defaultValue: 200,
 			control: { type: "range", min: 200, max: 600 },
 		},
 		height: {
-			defaultValue: 200,
 			control: { type: "range", min: 200, max: 600 },
 		},
 		alt: {
 			control: "string",
 		},
 		mode: {
-			defaultValue: "cover",
 			options: ["cover", "fill", "contain"],
 			control: { type: "inline-radio" },
 		},
 	},
+	args: {
+		lazy: false,
+		block: false,
+		src: "https://picsum.photos/200",
+		placeholder: "https://via.placeholder.com/200",
+		threshold: 0.5,
+		width: 200,
+		height: 200,
+		mode: "cover",
+	},
 };
 
 export const Default = (args) => {
-	return <>
-        <Image {...args} />
-        <Image {...args} />
-    </>
+	return <Image {...args} />;
+};
+
+export const Lazy = (args) => {
+	return (
+		<div>
+			{Array.from(new Array(20), (_, k) => k).map((i) => (
+				<Image {...args} lazy block src={`${args.src}?${i}`} key={i} />
+			))}
+		</div>
+	);
 };
